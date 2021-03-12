81ea87818a98d0e7ec11ba04e8d2ec5ace08fb63
Sittie <74464992+resilientmind@users.noreply.github.com>
2021-03-13 00:27:35 +0800

    Added about Sittie page

---
+++ aboutSittie.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>About Sittie</title>
+</head>
+<body     style= 'background-color: lightblue;
+        text-align: center;
+        margin-left: 90px;
+        margin-right: 90px; 
+        padding: 30px;
+        font-style: oblique; 
+        '>
+        
+        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQExNjAxV7Zq3Q/profile-displayphoto-shrink_800_800/0/1607960549131?e=1620864000&v=beta&t=9YO23iL3tGt9uu5E1esY_OGZGDRkRCu0jXSTuPUt7s8"  clear-left width="20%">
+        <br/>
+        <br/>
+
+    <?php
+        echo '<h1> Welcome to my page!</h1>';
+        echo 'Hi There!';
+
+        echo '<p>
+        I learned by watching videos in Youtube,reading related blog posts,taking my time searching the internet for free courses because I can not afford to buy paid courses. But then I realized it is not about buying courses or going to university,it is all about your perseverance and determination that will lead you to the path of success. In just a very short span of time, I was able to build website of my own.
+        <p/>';
+        echo '<br/>';
+        echo '<figure class="text-center">
+        <blockquote class="blockquote">
+        <p>We are bound only by the limits of our imagination.</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+        <cite title="Source Title">Misha Collins</cite>
+        </figcaption>
+        </figure>';
+        echo '<br/>';
+        
+
+    ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
