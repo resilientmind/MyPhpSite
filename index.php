3b8964f224a23647dd15a0829855e3aa2fbe5327
Sittie <74464992+resilientmind@users.noreply.github.com>
2021-03-13 00:26:22 +0800

    Added index page

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+       
+    <title>Resilient Mind</title>
+</head>
+
+<body>
+    
+
+
+    <div style= "background-color:lightblue;
+         text-align: center;
+         margin-left: 20px; 
+         padding: 90px";>
+    <div  class= "topleft">
+    <header style=>
+       <a href="https://www.linkedin.com/in/sittie-aguiman-62a16a1bb/">
+       <img src= "https://www.freeiconspng.com/thumbs/linkedin-icon/linkedin-icon-6.png"; padding: 85px; width='35px'>
+       </a>
+       <a href= "https://www.facebook.com/sittielawi/">
+       <img src= "https://img.flaticon.com/icons/png/512/124/124010.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF";padding: 35px; width='60px'>
+       </a><br/>
+       <div class="d-grid gap-2 d-md-block">
+       <button class="btn btn-primary" type="button">Learn more about Sittie</button>
+       </div>
+    </header>
+    </div>
+    <br/>
+   
+    <h1> Resilient Mind </h1><br/>
+    <?php
+      
+       echo '<br/>'; 
+       echo '<br/>';     
+       echo '<figure class="text-center">
+            <blockquote class="blockquote">
+            <p>If you are not willing to risk the unusual, you will have to settle for the ordinary.</p>
+            </blockquote>
+            <figcaption class="blockquote-footer">
+            <cite title="Source Title">Jim Rohn</cite>
+            </figcaption>
+            </figure>';
+            echo '<br/>';
+    
+   
+    echo       '<div id="carouselExampleInterval" class="carousel    
+                slide"data-bs-ride="carousel">         
+            <div class="carousel-inner">
+              <div class="carousel-item active" data-bs-interval="1700">
+                <img src="https://image.freepik.com/free-vector/woman-programmer-work-process-program-coding_24908-18757.jpg" class="d-block w-"20" alt="">
+               </div>
+              <div class="carousel-item" data-bs-interval="2000">
+                <img src="https://www.jotform.com/blog/wp-content/uploads/2020/03/WFH.blogbanner.png" class="d-block w-"20" alt="">
+              </div>
+              <div class="carousel-item" data-bs-interval="2700">
+                <img src="https://media.istockphoto.com/vectors/teenage-girk-working-on-computer-in-classroom-vector-id1150453750?k=6&m=1150453750&s=612x612&w=0&h=Mu_UNoQRacUqqnApWUgSGFCt8oixdwUVN2nGRm_DOkM=" class="d-block w-"20"  alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"  data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"  data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>';
+    
+        
+    ?>
+
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
